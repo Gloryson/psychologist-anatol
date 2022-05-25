d506e830f1e168ab7edefb0e2fb4df4defc6a5a7
Vlad Bolkunov <gloryson@mail.ru>
2022-05-25 10:10:16 +0300

    feat: add modal window for slider

--- about.js
+++ about.js
@@ -1,6 +1,8 @@
 const leftButton = document.querySelector('.slider__left-button');
 const rightButton = document.querySelector('.slider__right-button');
 const slides = document.querySelectorAll('.slide');
+const modalWindowSlide = document.querySelector('.modal__slide');
+const modalWindowSlideClose = document.querySelector('.modal__close');
 
 let x = 0;
 
@@ -22,4 +24,23 @@ leftButton.addEventListener('click', () => {
       slide.style.transform = `translateX(-${x}%)`
     })
   }
+})
+
+slides.forEach(slide => {
+  let index = slide.getAttribute('id');
+  slide.addEventListener('click', () => {
+    modalWindowSlide.classList.remove('off');
+    modalWindowSlideClose.classList.remove('off');
+    modalWindowSlide.style.backgroundImage = `url(./img/certificate${index}.jpg)`;
+    if (index < 5) {
+      modalWindowSlide.style.backgroundSize = '60vmin';
+    } else {
+      modalWindowSlide.style.backgroundSize = '80vmin';
+    }
+  })
+})
+
+modalWindowSlideClose.addEventListener('click', () => {
+    modalWindowSlide.classList.add('off');
+    modalWindowSlideClose.classList.add('off');
 })
